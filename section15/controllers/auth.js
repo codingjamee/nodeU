4a32f0af5fe3ac21c2624cbfc34f92058f08f106
jamee <codingjamee@gmail.com>
2023-04-25 13:24:13 +0900

    flash로 에러메시지 띄우고 삭제하기와 약간의 에러 수정

--- section15/controllers/auth.js
+++ section15/controllers/auth.js
@@ -3,18 +3,31 @@ const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
     //post login부분에서 명시한 key
-    errorMessage: req.flash("error"),
+    errorMessage: message,
   });
 };
 
 exports.getSignup = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render("auth/signup", {
     path: "/signup",
     pageTitle: "Signup",
+    errorMessage: message,
   });
 };
 
@@ -25,7 +38,6 @@ exports.postLogin = (req, res, next) => {
   User.findOne({ email: email })
     .then((user) => {
       if (!user) {
-        //알림이 저장될 key를 부여받게 됨
         req.flash("error", "Invalid email or password");
         return res.redirect("/login");
       }
@@ -42,6 +54,7 @@ exports.postLogin = (req, res, next) => {
               res.redirect("/");
             });
           }
+          req.flash("error", "Invalid email or password");
           res.redirect("/login");
         })
         .catch((err) => {
@@ -61,6 +74,7 @@ exports.postSignup = (req, res, next) => {
   User.findOne({ email: email })
     .then((userDoc) => {
       if (userDoc) {
+        req.flash("error", "Email exists already");
         return res.redirect("/signup");
       }
       //bcrypt의 hash메서드 :
