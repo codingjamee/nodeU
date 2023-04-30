2f33ea784794bf8c1c8f96168a618f558881921f
jamee <codingjamee@gmail.com>
2023-04-30 19:01:54 +0900

    로그인 실패시 입력한 데이터 유지시키기

--- section15/controllers/auth.js
+++ section15/controllers/auth.js
@@ -33,6 +33,11 @@ exports.getLogin = (req, res, next) => {
     pageTitle: "Login",
     //post login부분에서 명시한 key
     errorMessage: message,
+    oldInput: {
+      email: "",
+      password: "",
+    },
+    validationErrors: [],
   });
 };
 
@@ -50,7 +55,7 @@ exports.getSignup = (req, res, next) => {
     oldInput: {
       email: "",
       password: "",
-      confirmPassword: " ",
+      confirmPassword: "",
     },
     validationErrors: [],
   });
@@ -66,13 +71,26 @@ exports.postLogin = (req, res, next) => {
       path: "/signup",
       pageTitle: "Signup",
       errorMessage: errors.array()[0].msg,
+      oldInput: {
+        email: email,
+        password: password,
+      },
+      validationErrors: errors.array(),
     });
   }
   User.findOne({ email: email })
     .then((user) => {
       if (!user) {
-        req.flash("error", "Invalid email or password");
-        return res.redirect("/login");
+        return res.status(422).render("auth/login", {
+          path: "/login",
+          pageTitle: "login",
+          errorMessage: "Invalid email or password",
+          oldInput: {
+            email: email,
+            password: password,
+          },
+          validationErrors: [], //오류는 나더라도 어디서 나는지 확실히 하지 않음
+        });
       }
       bcrypt
         .compare(password, user.password)
@@ -87,8 +105,16 @@ exports.postLogin = (req, res, next) => {
               res.redirect("/");
             });
           }
-          req.flash("error", "Invalid email or password");
-          res.redirect("/login");
+          return res.status(422).render("auth/login", {
+            path: "/login",
+            pageTitle: "login",
+            errorMessage: "Invalid email or password",
+            oldInput: {
+              email: email,
+              password: password,
+            },
+            validationErrors: [], //오류는 나더라도 어디서 나는지 확실히 하지 않음
+          });
         })
         .catch((err) => {
           console.log(err);
