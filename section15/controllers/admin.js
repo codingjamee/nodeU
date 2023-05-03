cb23f8f0cdadc74cab68cbab4853cefa72d268b5
codingjamee <codingjamee@gmail.com>
2023-05-03 10:41:06 +0900

    multer로 유저에게 파일 전송 받아 저장해보기

--- section15/controllers/admin.js
+++ section15/controllers/admin.js
@@ -14,9 +14,10 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
-  const imageUrl = req.body.imageUrl;
+  const image = req.file;
   const price = req.body.price;
   const description = req.body.description;
+  console.log(image);
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
@@ -27,7 +28,7 @@ exports.postAddProduct = (req, res, next) => {
       hasError: true,
       product: {
         title: title,
-        imageUrl: imageUrl,
+        image: image,
         price: price,
         description: description,
       },
