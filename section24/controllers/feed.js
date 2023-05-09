e7dff927a42d13ce8d7ce16119763ff049fc8051
codingjamee <codingjamee@gmail.com>
2023-05-09 12:07:08 +0900

    프론트엔드와 통신하며 REST API 작성해보기

--- section24/controllers/feed.js
+++ section24/controllers/feed.js
@@ -1,6 +1,17 @@
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: "First Post", content: "This is the first post!" }],
+    posts: [
+      {
+        _id: "1",
+        title: "First Post",
+        content: "This is the first post!",
+        imageUrl: "images/He0747287a3724da5a2ff02823efabe313.jpg",
+        creator: {
+          name: "jenner",
+        },
+        createdAt: new Date(),
+      },
+    ],
   });
 };
 
@@ -9,6 +20,12 @@ exports.createPost = (req, res, next) => {
   const content = req.body.content;
   res.status(201).json({
     message: "Post created successfully!",
-    post: { id: new Date().toISOString(), title: title, content: content },
+    post: {
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: { name: "Jenner" },
+      createdAt: new Date(),
+    },
   });
 };
