db8a22ab5b17a250dbf45e6726df5982e1cba865
codingjamee <codingjamee@gmail.com>
2023-05-09 16:32:47 +0900

    커스텀 에러핸들링 함수, mongoose에 저장된 데이터 렌더링해주기

--- section24/app.js
+++ section24/app.js
@@ -1,11 +1,18 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const feedRoutes = require("./routes/feed");
+const MONGODB_URI = require("./dev").mongoURI;
+const mongoose = require("mongoose");
 
 const app = express();
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); //application/json
+
+// /images로 향하는 요청을 정적으로 제공하게 될 폴더
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -23,4 +30,16 @@ app.use((req, res, next) => {
 
 app.use("/feed", feedRoutes);
 
-app.listen(8080);
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
+
+mongoose
+  .connect(MONGODB_URI)
+  .then((result) => {
+    app.listen(8080);
+  })
+  .catch((err) => console.log(err));
